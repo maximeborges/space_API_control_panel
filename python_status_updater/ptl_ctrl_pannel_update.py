7aed081633cd702b61ae0cbf334ee68687a0d869
Alexandre <arekkusu@users.noreply.github.com>
2015-09-25 21:35:11 +0200

    New logic: Run once (and update status) then exist
    Python script logic is now run once, then exist. This is different from previous behavior which was to let the script running (with a loop).
    Also made some other changes (rename function, moves some code around, added comments)

--- python_status_updater/ptl_ctrl_pannel_update.py
+++ python_status_updater/ptl_ctrl_pannel_update.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# AR 2015-09-25
+# Script now run once and exit Intended for arduino with auto-reset disabled
+
 # AR 2015-04-13 v0.3
 # Constant SERIAL_DEV is now a tupple - allows trying to initiate with multiple dev name
 # Workaround for dev name changing from /dev/arduino0 to /dev/arduino1 (...)
@@ -13,36 +16,33 @@
 # Arduino sketch for control panel + more:
 # https://github.com/PostTenebrasLab/space_API_control_panel
 
+#Needed libraries
 import serial
 import time
 import sys
-
 import urllib
 import urllib2
-
 import logging
 
-##########
-
+#### CONSTANT #####
 # Loging
 LOG_FILE = "/var/log/ptl_ctrl_pannel_update/ptl_ctrl_pannel_update.py.log"
-LOG_LEVEL = logging.INFO
+DEBUG_LEVEL = logging.INFO
 # Implented debug level: logging.INFO, logging.ERROR, logging.DEBUG
 # Note: Debug will print API key to log file !
 
 # Serial
-SERIAL_DEV = ("/dev/arduino0", "/dev/arduino1", "/dev/arduino2", "/dev/arduino3", "/dev/arduino4")
+SERIAL_DEV = "/dev/arduino"
 SERIAL_RATE = 115200
 
 # Parameters for post request
 API_KEY = 'SECRET'
-URL = 'http://www.posttenebraslab.ch/status/change_status'
-REFRESH_DELAY = 60
+URL = 'http://...'
+########
 
-##########
-
+# Setup logging
 try:
-    logging.basicConfig(filename=LOG_FILE, level=LOG_LEVEL)
+    logging.basicConfig(filename=LOG_FILE, level=DEBUG_LEVEL)
     logging.info("Script starting: " +
                  (time.strftime("%a, %d %b %Y %H:%M:%S")))
 except Exception as error:
@@ -51,23 +51,22 @@ except Exception as error:
 
 # Setup serial port (this will reset Arduino)
 # Tries initiation with all device defined in tupple SERIAL_DEV
-for dev in SERIAL_DEV:
-    try:
-        logging.info(("Trying to connect to serial port " + str(dev) + " at " + str(SERIAL_RATE)))
-        ser = serial.Serial(dev, SERIAL_RATE)
-        ser.open()
-        ser.isOpen()
-        logging.info("Serial setup done")
-        break
-    except Exception as error:
-        print(error)
-        logging.error(
-            "Error when opening / initializing serial device")
+try:
+    dev = SERIAL_DEV
+    logging.info(("Trying to connect to serial port " + str(dev) + " at " + str(SERIAL_RATE)))
+    ser = serial.Serial(dev, SERIAL_RATE)
+    ser.open()
+    ser.isOpen()
+    logging.info("Serial setup done")
+except Exception as error:
+    print(error)
+    logging.error(
+        "Error when opening / initializing serial device")
 
-time.sleep(3)
+# Helper function 
 
 # Get value from control Panel right counter
-def get_value_1():
+def get_value_r():
     try:
         ser.write("get 1\n")
         return ser.readline()
@@ -77,7 +76,7 @@ def get_value_1():
     return None
 
 # Get value from control Panel left counter
-def get_value_2():
+def get_value_l():
     try:
         ser.write("get 2\n")
         return ser.readline()
@@ -87,7 +86,7 @@ def get_value_2():
     return None
 
 # Set value from control Panel right counter
-def set_value_1(i):
+def set_value_r(i):
     try:
         ser.write("set 1 " + str(i) + "\n")
     except Exception as error:
@@ -96,7 +95,7 @@ def set_value_1(i):
     return None
 
 # Set value from control Panel left counter
-def set_value_2(i):
+def set_value_l(i):
     try:
         ser.write("set 2 " + str(i) + "\n")
     except Exception as error:
@@ -104,67 +103,67 @@ def set_value_2(i):
         logging.error(error)
     return None
 
-# Main loop
-while True:
-    time.sleep(REFRESH_DELAY)
+# Main
 
-    value_1 = get_value_1()
-    value_2 = get_value_2()
+# Sleep and get value from Arduino
+time.sleep(2)
+value_r = get_value_r()
+value_l = get_value_l()
 
-    if value_1 is None or value_2 is None:
-        logging.error("Get Value failed (returned None). Will exit.")
-        sys.exit(1)
-        continue
-    else:
-        minute = int(value_1)
-        ppl_count = int(round(int(get_value_2()) / 10.0))
-        # Ppl count returns 10x the value. Need to be rounded off to closest
-        # number
+# Generate update text based on value
+if value_r is None or value_l is None:
+    logging.error("Get Value failed (returned None). Will exit.")
+    sys.exit(1)
+else:
+    minute = int(value_r)
+    ppl_count = int(round(int(get_value_l()) / 10.0))
+    # Ppl count returns 10x the value. rounded off to closest int
 
-    if minute == 0:
-        open_closed = "closed"
-        txt_open_closed = "The lab is closed."
-    elif minute > 0 and minute <= 60:
-        open_closed = "open"
-        txt_open_closed = "The lab is open - remaining time is " + \
-            str(minute) + " min."
-    elif minute > 60:
-        open_closed = "open"
-        hour = minute // 60
-        txt_open_closed = "The lab is open - planned to be open for " + \
-            str(hour) + " more hours."
+if minute == 0:
+    open_closed = "closed"
+    txt_open_closed = "The lab is closed."
+elif minute > 0 and minute <= 60:
+    open_closed = "open"
+    txt_open_closed = "The lab is open - remaining time is " + \
+        str(minute) + " min."
+elif minute > 60:
+    open_closed = "open"
+    hour = minute // 60
+    txt_open_closed = "The lab is open - planned to be open for " + \
+        str(hour) + " more hours."
 
-    if ppl_count == 0:
-        txt_ppl = "Nobody here !"
-    elif ppl_count == 1:
-        txt_ppl = "One lonely hacker in the space."
-    else:
-        txt_ppl = "There are " + str(ppl_count) + " hacker in the space."
+if ppl_count == 0:
+    txt_ppl = "Nobody here !"
+elif ppl_count == 1:
+    txt_ppl = "One lonely hacker in the space."
+else:
+    txt_ppl = "There are " + str(ppl_count) + " hacker in the space."
 
-    status_text = txt_open_closed + " " + \
-        txt_ppl + "  " + "[Set by PTL control panel]"
+status_text = txt_open_closed + " " + \
+    txt_ppl + "  " + "[Set by PTL control panel]"
 
-    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S"))
-    logging.info("Info used for update: ")
-    logging.info("minute: " + str(minute))
-    logging.info("ppl_count: " + str(ppl_count))
-    logging.info("status_text: " + str(status_text))
+#Some Logging
+logging.info(time.strftime("%a, %d %b %Y %H:%M:%S"))
+logging.info("Info used for update: ")
+logging.info("minute: " + str(minute))
+logging.info("ppl_count: " + str(ppl_count))
+logging.info("status_text: " + str(status_text))
 
-    values = {'api_key': API_KEY,
-              'open_closed': open_closed,
-              'status': status_text}
+#Send HTTP request to server to update status
+values = {'api_key': API_KEY,
+          'open_closed': open_closed,
+          'status': status_text}
 
-    try:
-        data = urllib.urlencode(values)
-        req = urllib2.Request(URL, data)
-        response = urllib2.urlopen(req)
-        the_page = response.read()
-    except Exception as error:
-        logging.error("Error while doing POST request to " + URL)
-        logging.debug("values used: " + str(values))
-        logging.error(error)
-        continue
+try:
+    data = urllib.urlencode(values)
+    req = urllib2.Request(URL, data)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+except Exception as error:
+    logging.error("Error while doing POST request to " + URL)
+    logging.debug("values used: " + str(values))
+    logging.error(error)
 
-    logging.debug("HTTP response page:")
-    logging.debug(the_page)
-    logging.debug("End of HTTP response")
+logging.debug("HTTP response page:")
+logging.debug(the_page)
+logging.debug("End of HTTP response")
